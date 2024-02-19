9c5fe86847514ff0aa5778e346003fe3b7cb3670
Muhammad Subhan <subhanahmed114624@gmail.com>
2024-02-19 04:36:51 +0000

    Added Main.py

--- main.py
+++ main.py
@@ -8,25 +8,16 @@ class MLOpsClass:
             raise ValueError("Enter number of students : ")
             print("Value cannot be negative ! ")
         self.total_strength += count
-
-        print("Students Enrolled successfully! ")
+        
     def dropStudents(self, count):
         if count < 0:
             raise ValueError("Number of students to drop must be non-negative.")
         if count > self.total_strength:
             raise ValueError("Cannot drop more students than enrolled.")
         self.total_strength -= count
-        print("Operation successfull! ")
         
     def getTotalStrength(self):
         return self.total_strength
 
     def getClassName(self):
         return self.class_name
-
-# if __name__ == "__main__":
-#     mlops_class = MLOpsClass()
-#     mlops_class.enrollStudents(25)
-#     print("Total strength after enrollment:", mlops_class.getTotalStrength())  # Output: 10
-#     mlops_class.dropStudents(3)
-#     print("Total strength after dropping students:", mlops_class.getTotalStrength())
